d7f3c74b061e48818ef5bc7f0d8e389b4a53b7de
jessgschueler <97937471+jessgschueler@users.noreply.github.com>
2022-07-14 16:19:07 -0700

    Update game_sales.sql

--- models/game_sales.sql
+++ models/game_sales.sql
@@ -10,7 +10,7 @@ select
     game_sales.NA_Sales	as na_sales,
     game_sales.EU_Sales as eu_sales,
     game_sales.JP_Sales as jp_sales,
-    game_sales.Other_Sales	as other_sales,
+    game_sales.Other_Sales as other_sales,
     game_sales.Global_Sales as global_sales
 
 from deb-01-346001.video_games.game_sales
